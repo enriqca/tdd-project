b614a12a76cfbd5d891d3d20f28a669977ab7980
Henrique César <rickcesaralves07@gmail.com>
2022-10-26 00:12:26 -0300

    Utilizando Testes Funcionais para planejar um MVP

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,38 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-browser = webdriver.Firefox()
+options = Options()
+options.binary = FirefoxBinary(r'C:\Program Files\Mozilla Firefox\firefox.exe')
+options.set_preference("browser.download.folderList",2)
+options.set_preference("browser.download.manager.showWhenStarting", False)
+options.set_preference("browser.download.dir","/Data")
+options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream,application/vnd.ms-excel")
+browser = webdriver.Firefox(executable_path=r'C:\Users\RICKC\Desktop\S.I\sem2022-2\TST\tdd-project/geckodriver.exe', options=options)
+
+# Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# Ela nota que o título da página menciona TODO
+assert 'To-Do' in browser.title
+
+# Ela é convidada a entrar com um item TODO imediatamente
+
+# Ela digita "Estudar testes funcionais" em uma caixa de texto
+
+# Quando ela aperta enter, a página atualiza, e mostra a lista
+# "1: Estudar testes funcionais" como um item da lista TODO
+
+# Ainda existe uma caixa de texto convidando para adicionar outro item
+# Ela digita: "Estudar testes de unidade"
+
+# A página atualiza novamente, e agora mostra ambos os itens na sua lista
+
+# Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
+# o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+
+# Ela visita a URL: a sua lista TODO ainda está armazenada
+
+# Satisfeita, ela vai dormir
+
+browser.quit()
